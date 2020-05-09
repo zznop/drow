5794dd121767bbe7db3eb2a7de8eb8b485fc3401
zznop <zznop0x90@gmail.com>
2020-05-09 17:21:02 -0400

    Wrote test payload
    Just prints lyrics to rappers delight :)

--- SConscript
+++ SConscript
@@ -10,6 +10,16 @@ stager_blob = env.Command(
     'objcopy -O binary --only-section=.text $SOURCE $TARGET'
 )
 
+# Build rappers delight blob
+
+rappers_delight_obj = env.Object('./src/arch/x86-64/rappers-delight.s')
+
+rappers_delight_blob = env.Command(
+    'rappers_delight.bin',
+    rappers_delight_obj,
+    'objcopy -O binary --only-section=.text $SOURCE $TARGET'
+)
+
 # Build drow
 
 sources = [
@@ -23,7 +33,6 @@ objs = env.Object(
     source=sources,
 )
 env.Depends(objs, stager_blob)
-
 
 drow = env.Program(
     source=objs,
--- src/arch/x86-64/stager.s
+++ src/arch/x86-64/stager.s
@@ -10,13 +10,19 @@ patch_ofs:
 .long 0
 
 past:
+    push rdi                   /* Preserve registers */
+    push rsi                   /*   ... */
+    push rdx                   /*   ... */
     call payload               /* Call the payload */
     mov eax, [rip + e_entry]   /* Get offset of real entry  */
     mov ebx, [rip + patch_ofs] /* Get offset of the stager */
     sub rbx, rax               /* Compute the delta between the stager and the real entry offsets */
-    lea rcx, [rip + base]      /* Get vaddr of the stager */
-    sub rcx, rbx               /* Subtract the delta from vaddr of the stager to get the vaddr of real entry */
-    jmp rcx                    /* Jump to real entry */
+    lea r10, [rip + base]      /* Get vaddr of the stager */
+    sub r10, rbx               /* Subtract the delta from vaddr of the stager to get the vaddr of real entry */
+    pop rdx                    /* Restore registers */
+    pop rsi                    /*   ... */
+    pop rdi                    /*   ... */
+    jmp r10                    /* Jump to real entry */
 
 payload:
 /* Payload blob is appended to the end of this blob */
