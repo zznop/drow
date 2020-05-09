ec9bfbea2ad46931c497d0faac443ab7c395aef9
Brandon Miller <brandonkmiller@protonmail.com>
2020-05-09 00:54:05 -0400

    mov->lea to get vaddr of stager base

--- src/arch/x86-64/stager.s
+++ src/arch/x86-64/stager.s
@@ -15,7 +15,7 @@ past:
     mov eax, [rip + e_entry]   /* Get offset of real entry  */
     mov ebx, [rip + patch_ofs] /* Get offset of the stager */
     sub rbx, rax               /* Compute the delta between the stager and the real entry offsets */
-    mov rcx, [rip + _start]    /* Get vaddr of the stager */
+    lea rcx, [rip + _start]    /* Get vaddr of the stager */
     sub rcx, rbx               /* Subtract the delta from vaddr of the stager to get the vaddr of real entry */
     jmp rcx                    /* Jump to real entry */
 
